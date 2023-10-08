0821d708711836e273f8dcb46e4848a3b29e51f9
matt <147349227+mattgithubmash@users.noreply.github.com>
2023-10-08 22:39:12 +0000

    Create tor.sh

---
+++ tor.sh
@@ -0,0 +1,4 @@
+wget https://n6.tg-drive.eu.org/488522/tor-browser-linux64-12.5.6_ALL.tar.xz?hash=73979ad617394b9f36e185fd
+tar -xf tor-browser-linux64-12.5.6_ALL.tar.xz\?hash\=73979ad617394b9f36e185fd 
+cd tor-browser
+./start-tor-browser.desktop
